ba47f64d3e77cc1cd2e20f34c1f5931917c93173
Seina <35910817+Seinay@users.noreply.github.com>
2018-10-27 14:41:56 +0800

    Update IFileService.java

--- src/main/java/com/mmall/service/IFileService.java
+++ src/main/java/com/mmall/service/IFileService.java
@@ -3,7 +3,7 @@ package com.mmall.service;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
- * Created by ClanceRen on 2018/4/24.
+ * Created by Seina on 2018/4/24.
  */
 public interface IFileService {
     String upload(MultipartFile file, String path);
