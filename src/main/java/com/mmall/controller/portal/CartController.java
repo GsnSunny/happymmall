f730282cfc8c411098e7310f1b2a78bc389269b0
Seina <35910817+Seinay@users.noreply.github.com>
2018-10-27 14:39:56 +0800

    Update CartController.java

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 
 /**
- * Created by ClanceRen on 2018/4/27.
+ * Created by Seina on 2018/4/27.
  */
 @Controller
 @RequestMapping("/cart")
